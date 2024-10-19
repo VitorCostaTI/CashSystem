7e6a7ef326b35095f715e0c0b23f4be1af23f25e
VitorCostaTI <vitortecnologia62@gmail.com>
2024-10-19 17:43:10 -0300

    MainBar

---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
