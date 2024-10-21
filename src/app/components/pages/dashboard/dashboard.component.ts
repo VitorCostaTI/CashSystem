40c58ddb08d2cb9c0ef30b0204be63bb15d7ed21
VitorCostaTI <vitortecnologia62@gmail.com>
2024-10-21 17:07:19 -0300

    Dashboard: Header

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    MatCardModule
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  visibility: boolean = false;
+
+  visibilityTrue() {
+    this.visibility = true;
+  }
+  visibility_off() {
+    this.visibility = false;
+  }
 }
