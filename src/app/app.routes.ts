5b9835118b24632a6848e5f4440cd969d513c8cb
VitorCostaTI <vitortecnologia62@gmail.com>
2024-10-28 03:17:44 -0300

    Refatoracao: Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/pages/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
